800d47c54bacab2f085e15c22b3261bad77173ea
s1phty <71193223+s1phty@users.noreply.github.com>
2020-11-15 00:04:58 -0800

    11/15/2020
    - Deleted 1CC command file

--- commands/1cc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: '1cc',
-    description: "this is a 1cc command!",
-    execute(message, args){
-        message.channel.send('Join 1CC for fun events, tournaments, and nitro prizes! discord.gg/AWjnTr9');
-    }
-}
