e845a9be9140f77e012bae6a5e0d2164388e3610
s1phty <71193223+s1phty@users.noreply.github.com>
2020-11-15 00:07:11 -0800

    11/15/2020 V2
    + Updated Instagram command to new handle

--- commands/instagram.js
+++ commands/instagram.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'instagram',
     description: "this is a instagram command!",
     execute(message, args){
-        message.channel.send('Follow the official 1v1.LOL Instagram page here: https://www.instagram.com/justbuild.lol/');
+        message.channel.send('Follow the official 1v1.LOL Instagram page here: https://www.instagram.com/1v1.lol_official/');
     }
 }
